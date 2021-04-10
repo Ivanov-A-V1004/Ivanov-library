bf61ea86b45716923539013fc1f139c3bc3c0873
Ivanov-A-V1004 <sawa.ivanov1004@mail.ru>
2021-04-10 11:15:12 +0300

    library2.1

--- tests/MyLogTest.php
+++ tests/MyLogTest.php
@@ -5,27 +5,13 @@ use Ivanov\MyLog;
 
 class MyLogTest extends TestCase
 {
-
-    public function testWrite()
-    {
-
-        $this->assertEquals('', MyLog::write("Testing"));
-        $this->assertEquals('', MyLog::write(1234));
-        $this->assertEquals('', MyLog::write());
-
+    public function testLog() {
+        $this->expectOutputString("This is a line equation!");
+        MyLog::log("This is a line equation!");
+        MyLog::write();
     }
 
-    public function testMyLogEx()
-    {
-        $this->expectException(TypeError::class);
-        $this->assertEquals('', MyLog::log());
-        $this->assertEquals('', MyLog::log(null));
-    }
-
-    public function testMyLog()
-    {
-        $this->assertEquals('', MyLog::log(1234));
-        $this->assertEquals('', MyLog::log(false));
-        $this->assertEquals('', MyLog::log("Testing"));
+    public function testInstance() {
+        $this->assertInstanceOf(MyLog::class, MyLog::Instance());
     }
 }
